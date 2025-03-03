985935fc49034c7c4cbdbee3e769c1c54352fd41
Deepraj Lodhi <deeprajlodhi59@gmail.com>
2025-03-03 16:40:13 +0530

    [Deepraj] Added: java program that use switch case statement to solve the problem

---
+++ Day01_JS_Programming_Constructs/nodeJS_programming_construct/switch_case_statements/read_number_display_day_name.js
@@ -0,0 +1,29 @@
+const prompt = require("prompt-sync")(); // Import prompt-sync for user input
+
+let dayNumber = parseInt(prompt("Enter a number (1-7): "));
+
+switch (dayNumber) {
+    case 1:
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Wednesday");
+        break;
+    case 5:
+        console.log("Thursday");
+        break;
+    case 6:
+        console.log("Friday");
+        break;
+    case 7:
+        console.log("Saturday");
+        break;
+    default:
+        console.log("Invalid input! Please enter a number between 1 and 7.");
+}
